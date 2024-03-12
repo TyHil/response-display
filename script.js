ca3c663f4607ba0c5a3934cd2674014a3ab9c6ba
Tyler Hill <gotyhill@gmail.com>
2024-03-12 00:57:34 -0500

    Published style guide

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const firebaseRef = firebase.database().ref();
 /* Tab Icon */
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-  document.querySelector('link[rel="icon"]').href = 'response-display/tabicon-light.png';
+  document.querySelector('link[rel="icon"]').href = '/response-display/tabicon-light.png';
 }
 
 /* Player/Operator */
